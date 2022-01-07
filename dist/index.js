5a86fe7bdb270bb47e4ba92b7bd7669ad923d46c
Patrick Byrne <patrick.byrne@catapultsports.com>
2022-01-07 14:45:41 +0000

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,24 @@
-function checkNumber(val) {
-    if (typeof val != "number") {
-        throw new Error("Not a number");
+function calculateTax(amount: number, format: boolean): string | number | null {
+    if (amount === 0) {
+        return null;
     }
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
-function calculateTax(amount) {
-    checkNumber(amount);
-    return amount * 1.2;
+
+let taxValue!: string | number | null;
+eval("taxValue = calculateTax(100, false)");
+
+if (taxValue !== null) {
+    let nonNullTaxValue: string | number = taxValue;
+    switch (typeof taxValue) {
+        case "number":
+            console.log(`Number Value: ${taxValue.toFixed(2)}`);
+            break;
+        case "string":
+            console.log(`String Value: ${taxValue.charAt(0)}`);
+            break;
+    }
+} else {
+    console.log("Value is not a string or a number");
 }
-let taxAmount = calculateTax(100);
-console.log(`Tax value: ${taxAmount}`);
